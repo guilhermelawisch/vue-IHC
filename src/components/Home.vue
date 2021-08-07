0f3f93bfb9b26156f41807945f708023deac82be
guilhermelawisch <lawisch03@gmail.com>
2021-08-07 14:54:35 -0300

    1 file modified

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>testing</p>
-  </div>
+  <header>
+    <h2>Interface Humano Computador</h2>
+  </header>
+  <main>
+  
+  </main>
+  <footer>
+    <a href="https://guilhermelawisch.vercel.app">by Guilherme Lawisch Machado &copy</a>
+  </footer>
 </template>
 
 <script>
@@ -16,5 +21,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  header {
+    position: fixed;
+    top: 0;
 
+    width: 100vw;
+    height: 10vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    border-bottom: 1px solid rgba(0,0,0,0.2)
+  }
+
+  footer {
+    position: fixed;
+    bottom: 0;
+
+    width: 100vw;
+    height: 10vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    border-top: 1px solid rgba(0,0,0,0.2)
+  }
+  footer a {
+    text-decoration: none;
+    color: rgb(0,0,0)
+  }
 </style>
