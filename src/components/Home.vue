fa3a09c960d392718febdd89f47e68a0ca6dd66e
guilhermelawisch <lawisch03@gmail.com>
2021-08-08 01:50:12 -0300

    1 file modified

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -73,9 +73,17 @@
           <label for="phone">Phone:</label>
           <input type="text" name="phone" id="phone" placeholder="11 4002 8922">
         </div>
-        <div>
+        <div class="cep">
           <label for="cep">CEP:</label>
-          <input type="text" name="cep" id="cep" placeholder="00000-000" v-model="cep">
+          <div>
+            <input type="text" name="cep" id="cep" placeholder="00000-000" v-model="cep">
+            <button @click="getAddress(event)">
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512">
+                <path d="M496.131 435.698l-121.276-103.147c-12.537-11.283-25.945-16.463-36.776-15.963 28.628-33.534 45.921-77.039 45.921-124.588 0-106.039-85.961-192-192-192s-192 85.961-192 192 85.961 192 192 192c47.549 0 91.054-17.293 124.588-45.922-0.5 10.831 4.68 24.239 15.963 36.776l103.147 121.276c17.661 19.623 46.511 21.277 64.11 3.678s15.946-46.449-3.677-64.11zM192 320c-70.692 0-128-57.308-128-128s57.308-128 128-128 128 57.308 128 128-57.307 128-128 128z"></path>
+              </svg>
+            </button>
+          </div>
+          
         </div>
         <div class="address-one">
           <div>
@@ -110,9 +118,6 @@
         <div>
           <label for="linkedin">LinkedIn:</label>
           <input type="text" name="linkedin" id="linkedin" placeholder="https://linkedin/in/exemplo">
-        </div>
-        <div id='test'>
-          <button @click="getAddress(event)">Test</button>
         </div>
       </form>
     </div>
@@ -210,6 +215,21 @@ export default {
   }
   main .right .names div:last-child {
     flex: 1;
+  }
+  main .right .cep div {
+    margin: 0;
+    position: relative;
+  }
+  main .right .cep div button {
+    position: absolute;
+    right: 10px;
+    margin: 16px 0;
+
+    border: 0;
+    background: rgba(0,0,0,0)
+  }
+  main .right .cep div button:hover {
+    cursor: pointer;
   }
   main .right .address-one {
     display: flex;
